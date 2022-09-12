8f10d13f0762f2a9713467b3ffcb9daed894c075
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-09-12 15:42:05 +0100

    refactored enums

--- src/main/java/com/example/musicapi/models/Genre.java
+++ src/main/java/com/example/musicapi/models/Genre.java
@@ -1,16 +1,32 @@
 package com.example.musicapi.models;
 
 public enum Genre {
-    BLUES,
-    COUNTRY,
-    CONTEMPORARY,
-    ELECTRONIC,
-    FOLK,
-    HIPHOP,
-    JAZZ,
-    POP,
-    RNB,
-    SOUL,
-    ROCK,
-    METAL
+    BLUES("Blues"),
+    COUNTRY("Country"),
+    CONTEMPORARY("Contemporary"),
+    ELECTRONIC("Electronic"),
+    FOLK("Folk"),
+    HIPHOP("Hip-hop"),
+    JAZZ("Jazz"),
+    POP("Pop"),
+    FUNK("Funk"),
+    RNB("RnB"),
+    SOUL("Soul"),
+    ROCK("Rock"),
+    METAL("Metal"),
+    ALTERNATIVE("Alternative");
+
+    private String value;
+
+    Genre(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
