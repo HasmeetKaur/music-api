3f8356a1d84867031c7894d50446bc0bb050fe7f
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-12 10:03:05 +0100

    inital commit

---
+++ src/main/java/com/example/musicapi/MusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musicapi/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
