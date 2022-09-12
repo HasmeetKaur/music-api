c1a91b97c63decf8fe4ad78efd5755d9f9dc2f93
Faizan Khan <faizanahmkhan@gmail.com>
2022-09-12 18:05:59 +0100

    working on Album Services

--- src/main/java/com/example/musicapi/repositories/AlbumRepository.java
+++ src/main/java/com/example/musicapi/repositories/AlbumRepository.java
@@ -1,5 +1,8 @@
 package com.example.musicapi.repositories;
 
+import com.example.musicapi.models.Album;
+import com.example.musicapi.models.Artist;
+import com.example.musicapi.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +12,7 @@ public interface AlbumRepository extends JpaRepository<Album, Long> {
     List<Album> findByArtistId(Long id);
     List<Album> findByTrackId(Long id);
 
+    List<Album> findByGenre(Genre genre);
+
+    List<Album> findByArtist(Artist artist);
 }
--- src/main/java/com/example/musicapi/repositories/UserRepository.java
+++ src/main/java/com/example/musicapi/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.musicapi.repositories;
 
+import com.example.musicapi.models.Album;
 import com.example.musicapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<Album> findByUserID(long id);
 }
