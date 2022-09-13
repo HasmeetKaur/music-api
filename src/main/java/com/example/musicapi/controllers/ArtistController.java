f456f483564dc4bd636d7cb0aefb7e7e4f0f3424
Faizan Khan <faizanahmkhan@gmail.com>
2022-09-13 12:35:38 +0100

    completed Album & Artist Controller, need to look over

--- src/main/java/com/example/musicapi/controllers/ArtistController.java
+++ src/main/java/com/example/musicapi/controllers/ArtistController.java
@@ -1,9 +1,6 @@
 package com.example.musicapi.controllers;
 
-import com.example.musicapi.models.Artist;
-import com.example.musicapi.models.Genre;
-import com.example.musicapi.models.Reply;
-import com.example.musicapi.models.User;
+import com.example.musicapi.models.*;
 import com.example.musicapi.services.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -33,10 +30,31 @@ public class ArtistController {
 
     // ERROR - need to find notation for enum
     @GetMapping(value = "/{genre}")
-    public ResponseEntity<Artist> getArtistByGenre(@PathVariable Genre genre) {
-        return artistService.equals(getArtistByGenre(genre)) ? new ResponseEntity<>(artistService.getArtistByGenre(genre).get(artist), HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    public ResponseEntity<List<Artist>> getArtistByGenre(@PathVariable Genre genre) {
+        return artistService.equals(getArtistByGenre(genre)) ? new ResponseEntity<>(artistService.getArtistByGenre(genre), HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Artist>> getFavouriteArtistByUserId(@PathVariable Long id){
+        return artistService.equals(getFavouriteArtistByUserId(id)) ? new ResponseEntity<>(artistService.getFavouriteArtistsByUserId(id), HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
 
+    @PostMapping
+    public ResponseEntity<Artist> saveArtist(@RequestBody Artist artist) {
+        Artist newArtist = artistService.saveArtist(artist);
+        return new ResponseEntity<>(artist, HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<Optional<Artist>> searchArtistByName(@RequestBody String name) {
+        Optional<Artist> artist = artistService.searchArtistByName(name);
+        return new ResponseEntity<>(artist, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/artists/{artistsId}")
+    public ResponseEntity<String> removeArtistById(@PathVariable long artistId) {
+        Reply reply = artistService.removeArtistById(artistId);
+        return reply.isPassed() ? new ResponseEntity<>(reply.getMessage(), HttpStatus.OK) : new ResponseEntity<>(reply.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
 }
