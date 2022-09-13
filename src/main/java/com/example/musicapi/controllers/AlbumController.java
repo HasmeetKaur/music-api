b2f2a701d49020f6fa3fb5a390e2a60407251669
Faizan Khan <faizanahmkhan@gmail.com>
2022-09-13 15:04:12 +0100

    refactored album controller, repo, service

--- src/main/java/com/example/musicapi/controllers/AlbumController.java
+++ src/main/java/com/example/musicapi/controllers/AlbumController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.apache.logging.log4j.ThreadContext.isEmpty;
 
 @RestController
-@RequestMapping(varlue = "/albums")
+@RequestMapping(value = "/albums")
 public class AlbumController {
 
     @Autowired
