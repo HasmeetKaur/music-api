893c3a5be48b5008e28f9f210c0a34006a0e8631
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-13 09:15:51 +0100

    artist service added

--- src/main/java/com/example/musicapi/repositories/ArtistRepository.java
+++ src/main/java/com/example/musicapi/repositories/ArtistRepository.java
@@ -1,12 +1,27 @@
 package com.example.musicapi.repositories;
 
 import com.example.musicapi.models.Artist;
+import com.example.musicapi.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     List<Artist> findByTrackId(Long id);
     List<Artist> findByAlbumId(Long id);
+
+    List<Artist> findByUserId();
+    
+    List<Artist> findById();
+    List<Artist> findByGenre(Genre genre);
+
+    List<Artist> findFavouriteArtistByUserId(long id);
+
+    List<Artist> saveArtist(Artist artist);
+
+    Optional<Artist> removeArtistById(long id);
+
+    Optional<Artist> searchArtistByName(String name);
 }
--- src/main/java/com/example/musicapi/services/ArtistService.java
+++ src/main/java/com/example/musicapi/services/ArtistService.java
@@ -1,21 +1,43 @@
 package com.example.musicapi.services;
 
 import com.example.musicapi.models.Artist;
+import com.example.musicapi.models.Genre;
+import com.example.musicapi.repositories.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 public class ArtistService {
 
-    public List<Artist> getAllArtists() {
+    @Autowired
+    ArtistRepository artistRepository;
+
+    public List<Artist> getAllArtist() {
+        return artistRepository.findAll();
     }
 
-    public List<Artist> getArtistsById() {
+    public Optional<Artist> getArtistById(long id) {
+        return artistRepository.findById(id);
     }
 
-    public List<Artist> getArtistsByGenre() {
+    public List<Artist> getArtistByGenre(Genre genre) {
+        return artistRepository.findByGenre(genre);
     }
 
-    public List<Artist> getFavouriteArtistsByUserId() {
+    public List<Artist> getFavouriteArtistByUserId(long id) {
+        return artistRepository.findFavouriteArtistByUserId(id);
+    }
+
+    public List<Artist> saveArtist(Artist artist) {
+        return artistRepository.saveArtist(artist);
+    }
+
+    public Optional<Artist> removeArtistById(long id) {
+        return artistRepository.removeArtistById(id);
+    }
+
+    public Optional<Artist> searchArtistByName(String name) {
+        return artistRepository.searchArtistByName(name);
     }
 }
